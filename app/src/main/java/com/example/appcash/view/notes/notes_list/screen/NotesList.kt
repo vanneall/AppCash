81331233c0bd9ed664854c6c93e3dd3626bdaeb3
Ivan Demin <defoxyweex@gmail.com>
2024-04-01 11:56:16 +0300

    Обновлен ui страницы списка заметок

--- app/src/main/java/com/example/appcash/view/notes/notes_list/screen/NotesList.kt
+++ app/src/main/java/com/example/appcash/view/notes/notes_list/screen/NotesList.kt
@@ -2,40 +2,36 @@
 
 package com.example.appcash.view.notes.notes_list.screen
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.appcash.navigation.Destinations.NOTE_SCREEN
-import com.example.appcash.utils.OpenParamsMagicNumbers.OPEN_FOR_CREATE
 import com.example.appcash.utils.events.Event
-import com.example.appcash.view.general.list.Header
-import com.example.appcash.view.general.other.FolderSettingsModalBottomSheet
-import com.example.appcash.view.general.other.SearchTextField
-import com.example.appcash.view.notes.note_info.components.NoteOpenMode.CREATE
-import com.example.appcash.view.notes.note_info.components.NoteOpenMode.EDIT
 import com.example.appcash.view.notes.notes_list.components.NotesListState
+import com.example.appcash.view.ui.theme.Gray
+import com.example.appcash.view.ui.theme.LightGray
 
 @Composable
 fun NotesList(
@@ -44,89 +40,82 @@ fun NotesList(
     navigateTo: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val sheetState = rememberModalBottomSheetState()
-
-    Box(
-        modifier = Modifier.fillMaxSize()
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(15.dp),
-            modifier = modifier
-        ) {
-            item {
-                Header(
-                    name = state.folderName,
-                    modifier = Modifier.padding(vertical = 20.dp)
-                )
-            }
-
-            item {
-                SearchTextField(
-                    searchQuery = state.searchQuery,
-                    onEvent = onEvent,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 10.dp)
-                )
-            }
-
-            itemsIndexed(
-                items = state.notesList,
-                key = { _, note -> note.id }
-            ) { index, note ->
-                ListItem(
-                    title = note.title,
-                    content = note.content,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { navigateTo("$NOTE_SCREEN/${state.folderId}/${note.id}/${EDIT.name}") }
-                )
-
-                Divider(
-                    thickness = 1.dp,
-                    color = Color.LightGray,
-                    modifier = Modifier.padding(top = 15.dp)
-                ).takeIf { index < state.notesList.size - 1 }
-            }
-        }
-
-        FloatingActionButton(
-            onClick = { navigateTo("$NOTE_SCREEN/${state.folderId}/$OPEN_FOR_CREATE/${CREATE.name}") },
-            shape = CircleShape,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(end = 30.dp, bottom = 70.dp)
-                .size(70.dp)
-        ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null)
-        }
-
-        if (state.showEdit) {
-            FolderSettingsModalBottomSheet(
-                sheetState = sheetState,
-                onEvent = onEvent,
-                modifier = Modifier.fillMaxWidth()
+        items(
+            items = state.notesList,
+            key = { item -> item.id },
+        ) { item ->
+            NoteListItem(
+                title = item.title,
+                content = item.content,
+                modifier = Modifier
+                    .size(width = 358.dp, height = 64.dp)
+                    .background(
+                        color = LightGray, shape = RoundedCornerShape(20.dp)
+                    )
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
             )
         }
     }
 }
 
 @Composable
-private fun ListItem(
+private fun NoteListItem(
     title: String,
     content: String,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = title,
-            fontSize = 17.sp,
-            color = Color.Black
-        )
-        Text(
-            text = content,
-            fontSize = 12.sp,
-            color = Color.Gray
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.width(286.dp)
+        ) {
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Black,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = content,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Gray,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = "Иконка больше",
+            modifier = Modifier
+                .size(24.dp)
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NoteListPreview() {
+    NoteListItem(
+        title = "Разработка мобильного приложения",
+        content = "Цель проекта - создание удобного и функционального приложения",
+        modifier = Modifier
+            .size(width = 358.dp, height = 64.dp)
+            .background(
+                color = LightGray, shape = RoundedCornerShape(20.dp)
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
 }
